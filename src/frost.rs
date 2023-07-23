82b75056986feac9aceff8fa8bdadd5d1193aa51
Erwan <erwanor@penumbralabs.xyz>
2023-07-22 23:55:21 -0400

    freddo: implement schnorr sig

--- src/frost.rs
+++ src/frost.rs
@@ -1,4 +1,5 @@
-use ark_ff::{UniformRand, Zero};
+use ark_ff::{PrimeField, UniformRand, Zero};
+use decaf377::Fr;
 use rand_core::{CryptoRng, RngCore};
 
 use crate::schnorr;
@@ -79,6 +80,8 @@ pub struct FrostSigner {
     /// The coefficient of the polynomial used to generate the signing key.
     /// TODO(erwan): newtype this or something.
     pub signing_key: Vec<decaf377::Fr>,
+    /// This participant's random nonce.
+    pub nonce: decaf377::Fr,
     /// A public commitment to the coefficients of the polynomial
     /// used to generate the signing key.
     pub commitment: Vec<decaf377::Element>,
@@ -122,10 +125,13 @@ impl FrostSigner {
             .map(|k| k * generator)
             .collect::<Vec<_>>();
 
+        let nonce = decaf377::Fr::rand(&mut rng);
+
         Self {
             id,
             signing_key,
             commitment,
+            nonce,
         }
     }
 
@@ -137,8 +143,31 @@ impl FrostSigner {
         self.commitment.clone()
     }
 
-    pub fn schnorr_sign(&self, transcript: merlin::Transcript) -> schnorr::Signature {
-        schnorr::Signature::default()
+    pub fn schnorr_sign(&self) -> (merlin::Transcript, schnorr::Signature) {
+        let mut transcript = merlin::Transcript::new(b"freddo377-key-gen");
+        transcript.append_u64(b"signer-identifier", self.id());
+        transcript.append_message(
+            b"constant-term-commitment",
+            self.commitment[0].vartime_compress().0.as_ref(),
+        );
+
+        let nonce_commitment = self.nonce * decaf377::basepoint();
+        transcript.append_message(
+            b"nonce-commitment",
+            nonce_commitment.vartime_compress().0.as_ref(),
+        );
+
+        let mut challenge_raw: [u8; 32] = [0u8; 32];
+        transcript.challenge_bytes(b"schnorr-sig-challenge", &mut challenge_raw);
+
+        let challenge_scalar = decaf377::Fr::from_le_bytes_mod_order(&challenge_raw);
+
+        let sig = schnorr::Signature {
+            commitment: nonce_commitment,
+            challenge_response: self.nonce + challenge_scalar * self.signing_key[0],
+        };
+
+        (transcript, sig)
     }
 }
 
